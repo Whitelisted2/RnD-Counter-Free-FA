2ba4f65d546aee9d8413a0f1aa97b21c8cd31cfe
Siddharth P <90827725+Whitelisted2@users.noreply.github.com>
2023-12-01 22:13:24 +0530

    clean from sys-side

--- drawautomaton.py
+++
@@ -1,5 +0,0 @@
-from graphviz import *
-dotstring_path = "sandbox/output/newfolder/myregex1.dotstring"
-
-
-render('dot', 'png', dotstring_path).replace('\\', '/')
--- newregexparser.py
+++ newregexparser.py
@@ -1,4 +1,4 @@
-### for the property file with regex in it.
+### for the property file with one angular-bracket-delimited regex in it.
 # first, must make the 'inductive' form of regex. then, put into GAP and manipulate
 # this is a bit more robust than regexparser.py
 
@@ -54,12 +54,9 @@ def IsAperiodicExp(line):
 
     LoadPackage(\"automata\");;
     LoadPackage(\"semigroup\");;
-    LoadPackage(\"datastructures\");;
 
     rat := """+ line +""";
-    # Print(\"\\n----\");
     Print(rat);
-    # Print(\"|\");
     if flag then PrintTo(\"sandbox/tmp/temp1.ratex\", rat); fi;
 
     ss := SyntacticSemigroupLang(rat);;
@@ -99,29 +96,15 @@ def IsAperiodicExp(line):
     else
         # periodic
         Print(\"\\nPeriodic: \");
-        # Print(\"\\nSyntactic Semigroup: \");
-        # Print(ss);
 
         if flag then PrintTo(\"sandbox/tmp/temp1.automaton\", aut); fi;
         n := NumberStatesOfAutomaton(aut);
         ts := TransitionSemigroup(aut);
-        # Print(aut);
         ss3 := SemigroupByGenerators(ts);
         gen := GeneratorsOfSemigroup(ss3);
         mon := MonoidByGenerators(gen);
-
-        # Print(\"\\nTransition Semigroup: \");
-        # Print(ts);
-        # Print(\"\\n\");
-
-        # Print(\"\\nMultiplication Table: \\n\");
         mult := MultiplicationTable(mon);
         if flag then PrintTo(\"sandbox/tmp/temp1.mult\", mult); fi;
-        # OutputLogTo(\"sandbox/PeriodicList.txt\");
-        # Print(mon);
-        # OutputLogTo();
-
-        # Print(\"\\n\");
         
         tsset := Elements(ts);
         if flag then PrintTo(\"sandbox/tmp/temp1.semigroup\", tsset); fi;
@@ -131,30 +114,17 @@ def IsAperiodicExp(line):
         real_gen := GeneratorsOfSemigroup(ts);
         # each ele of realgen to be enumerated to disambiguate the list... s, p, sp, ss, ps, pp, ....
 
-        # Print(real_gen);
-        # Print(tsset);
-        # open := PlistDeque();
-
         open := [];
         closed := [];
-        # curr_path := [];
         hash := HashMap();
         for ele in real_gen do                 # init open list
             Append(open, [ele]);
             hash[ele] := [Position(real_gen, ele)];
             # Append(closed, [ele]);
         od;
-        # Print(\"\\nOpen:\");
-        # Print(open);
-        # Print(\"\\nClosed:\");
-        # Print(closed);
-        # for ele in open do
-        #     Print(\"\\nElement: \", ele, \" id = \", hash[ele]);
-        # od;
+
         while (not IsEmpty(open)) do
-            # Print(\"\\nreenter\");
             curr := Remove(open,1);             # s p
-            # Print(open);
             curr_path := hash[curr];
             for ele in real_gen do
                 child := curr*ele; # hashmap bfs
@@ -164,32 +134,22 @@ def IsAperiodicExp(line):
                 # Print(\"\\ntemp = \", curr_path);
                 Append(temp, [Position(real_gen, ele)]);
                 if ((not (child in Keys(hash))) and (not (child in real_gen)) and (not (child = curr))) then
-                    # Print(\" yes \");
                     hash[child] := temp;
-                    # Print(\"\\nhash[\", child, \"] =\", temp);
                 fi;
                 if ((not (child in open)) and (not (child in closed)) and (not (child = curr)) and (not (child in real_gen))) then
                     Append(open, [child]);
-                    # Print(\"\\nFor \", child ,\", unique moment=\");
-                    # Print(hash[child]);
                 else
                     # Print(\"\\nhey! hash=\");
-                    # Print(hash[child]);
                 fi;
             od;
             Append(closed, [curr]);
-            # Print(\"\\nOpen:\");
-            # Print(open);
-            # Print(\"\\nClosed:\");
-            # Print(closed);
-            
         od;
         
         
         # for ele in closed do
         #     Print(\"\\nElement: \", ele, \" id = \", hash[ele]);
         # od;
-        ##################################
+
         OutputLogTo(\"sandbox/PeriodicList.txt\");;
             Print(n);;
             Print(\"\\n\");;
@@ -207,11 +167,10 @@ def IsAperiodicExp(line):
     quit;
     EOI
     """
-    # print(cmd)
+    
     output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
     output = output.decode('utf-8')
     print(output)
-    # print("Processing ratex [", rid+1, "]")
 
     output_arr = output.split("\n")
 
@@ -223,9 +182,7 @@ def IsAperiodicExp(line):
         """
         output2 = subprocess.check_output(cmd2, shell=True, stderr=subprocess.STDOUT)
         output2 = output2.decode('utf-8')
-        # print("_______________________________________")
         print(output2)
-        # print("_______________________________________")
 
     else:
         decision = "aperiodic"
@@ -237,23 +194,20 @@ def IsAperiodicExp(line):
 
 
 with open("sandbox/output.txt", "r") as file:
+    # open the regex inductive file
     lines = file.read()
     lines = lines.split("\n")
     for line in lines:
-        # print(line)
         if line != "":
             IsAperiodicExp(line)
 
 if want_excel:
        workbook = openpyxl.Workbook()
-       # workbook = openpyxl.load_workbook('sandbox/output.xlsx')
        sheet = workbook.active
        sheet["A1"] = "Rational Expression"
        sheet["B1"] = "Aperiodic?"
     #    sheet["C1"] = "Note: The 'false' ones may not have been processed by gap (in the current format) properly."
        for i in range(2, rid+2):
-              # print(ratex_dict[i][0])
-              # print(ratex_dict[i][1])
               sheet["A"+str(i)] = ratex_dict[i-1][0]
               sheet["B"+str(i)] = ratex_dict[i-1][1]
        # if os.path.exists('sandbox/output.xlsx'):
